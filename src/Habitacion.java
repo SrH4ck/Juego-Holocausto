307d360400be4c79bf2119bad8ad1d8d7903be81
Antonio Mesa <antonio.iesgerena@gmail.com>
2024-07-02 19:33:09 +0200

    refactorización de código y se han añadido comentarios al código

--- src/Habitacion.java
+++ src/Habitacion.java
@@ -1,7 +1,9 @@
 public class Habitacion {
 
-    public static final int ANCHO=10;
-    public static final int ALTO=10;
+    // asignamos el tamaño del mapa modificando estas dos propiedades
+    public static final int ANCHO=20;
+    public static final int ALTO=20;
+
     private Posicion puertaEntrada;
     private static Posicion puertaSalida;
     private Jugador jugador;
@@ -24,7 +26,7 @@ public class Habitacion {
             this.puertaEntrada = puertaEntrada;
         else throw new PuertaColocadaEnLocalizacionIncorrecta("ERROR!!\n" +
                 "Puerta de Entrada colocada en localización incorrecta\n" +
-                "Valores permitidos: eje Y = 9 y eje x  = 1-8");
+                "Valores permitidos: eje Y = "+(Habitacion.ALTO -1) + "y eje x  = 1-"+(Habitacion.ALTO -2));
     }
 
     public Posicion getPuertaSalida() {
@@ -33,16 +35,17 @@ public class Habitacion {
 
     public void setPuertaSalida(Posicion puertaSalida) {
         //Valores permitidos: 1-8 en eje Y eje X 9 ó 0
-        if ((ALTO-1 != puertaSalida.getPosY() && puertaSalida.getPosY()!=0) && (puertaSalida.getPosX() == 0 || puertaSalida.getPosX() == ANCHO -1))this.puertaSalida = puertaSalida;
+        if ((ALTO-1 != puertaSalida.getPosY() && puertaSalida.getPosY()!=0) && (puertaSalida.getPosX() == 0 || puertaSalida.getPosX() == ANCHO -1))
+            Habitacion.puertaSalida = puertaSalida;
         else throw new PuertaColocadaEnLocalizacionIncorrecta("ERROR!!\n" +
                 "Puerta de salida colocada en localización incorrecta\n" +
-                "Valores permitidos: 1-8 en eje Y eje X 9 ó 0");
+                "Valores permitidos: 1-"+ (Habitacion.ALTO -2) +" en eje Y eje X "+(Habitacion.ANCHO -1) +" ó 0");
     }
     public void setJugador(Jugador j){
         if ((j.getPosicion().getPosX() != 0 && j.getPosicion().getPosX() != ANCHO -1) && (j.getPosicion().getPosY() != 0 && j.getPosicion().getPosY() != ALTO -1)){
             this.jugador = j;
         }else throw new JugadorColocadoIncorrectamente("Jugador Colocado en posición incorrecta\n" +
-                "Valores Correctos: X = 1-8 Y = 1-8");
+                "Valores Correctos: X = 1-" + (Habitacion.ANCHO -2) +" Y = 1-"+ (Habitacion.ALTO -2));
 
     }
     public void setPersonaje(Personaje p){
--- src/Main.java
+++ src/Main.java
@@ -6,55 +6,92 @@ public class Main {
         int accionJuego = -1;
         Scanner sc = new Scanner(System.in);
         try {
-            Posicion puertaEntrada = new Posicion(1,9);
-            Posicion jugadorPuertaEntrada = new Posicion(8,1);
-
-            Jugador jugador = new Jugador(jugadorPuertaEntrada);
-
+            // Creamos la habitación
             Habitacion habitacion = new Habitacion();
+
+            //Asignamos la posición de las puertas en la habitación
+            Posicion puertaEntrada = new Posicion(1,Habitacion.ALTO-1);
+            Posicion puertaSalida = new Posicion(Habitacion.ANCHO-1,1);
+
+            // se resta 1 para que se coloque justamente delante y "dentro"
+            Posicion jugadorPuertaEntrada = new Posicion(puertaEntrada.getPosX(), puertaEntrada.getPosY()-1);
+
+            //Posicionamos al jugador
+            Jugador jugador = new Jugador(jugadorPuertaEntrada);
             habitacion.setJugador(jugador);
 
+            //Posicionamos la puerta de entrada
             habitacion.setPuertaEntrada(puertaEntrada);
-            Posicion puertaSalida = new Posicion(9,1);
+
+            //Posicionamos la puerta de salida
             habitacion.setPuertaSalida(puertaSalida);
 
             while (accionJuego != Juego.SALIR_DEL_JUEGO){
                 Juego.pintarHabitacion(habitacion);
                 Juego.pintarMenu();
+
+                //Recogemos la opción deseada por el usuario
                 accionJuego = sc.nextInt();
+
                 switch (accionJuego){
                     case Juego.SALIR_DEL_JUEGO:
                         System.out.println("Saliendo del juego...");
                         break;
                     case Juego.LANZAR_DADO:
                         //Lanzar el dado de movimiento
-                        int tiradaDado = jugador.lanzarDado(dado);
+                        int movimientosPosibles = jugador.lanzarDado(dado);
+
+                        //Mensajes informativos
                         System.out.println("Valor del dado lanzado: \n" +
-                                "Dado --> " + tiradaDado);
-                        System.out.println("Números de movimiento en horizontal " + tiradaDado);
-                        System.out.println("Números de movimiento en vertical " + tiradaDado);
+                                "Dado --> " + movimientosPosibles);
+                        System.out.println("Números de movimiento en horizontal " + movimientosPosibles);
+                        System.out.println("Números de movimiento en vertical " + movimientosPosibles);
                         System.out.println("\nCuantos movimientos quieres gastar para moverte en horizontal número positivo \n" +
                                 "para moverte  a la derecha , número negativo para moverte a la izquierda");
+
+                        // Leemos número del usuario
                         int numMovX = sc.nextInt();
-                        if (Math.abs(numMovX) > tiradaDado){
+
+                        //verificamos que el número de movimientos que quiere el usuario no es mayor al permitido
+                        if (Math.abs(numMovX) > movimientosPosibles){
+
                             System.out.println("Has introducido un movimiento mayor al permitido");
+
                         }else{
-                            tiradaDado -= Math.abs(numMovX);
-                            System.out.println("Números de movimiento en horizontal " + tiradaDado);
-                            System.out.println("Números de movimiento en vertical " + tiradaDado);
+
+                            //si no es mayor realizamos el decremento de posibles movimientos
+                            movimientosPosibles -= Math.abs(numMovX);
+
+                            //Mensajes informativos
+                            System.out.println("Números de movimiento en horizontal " + movimientosPosibles);
+                            System.out.println("Números de movimiento en vertical " + movimientosPosibles);
                             System.out.println("\nCuantos movimientos quieres gastar para moverte en vertical número positivo\n" +
                                     "para moverte hacia abajo, número negativo para moverte hacia arriba");
+
+                            // Leemos número del usuario
                             int numMovY = sc.nextInt();
-                            if (Math.abs(numMovY) > tiradaDado){
+
+                            //verificamos que el número de movimientos que quiere el usuario no es mayor al permitido
+                            if (Math.abs(numMovY) > movimientosPosibles){
+
                                 System.out.println("\nHas introducido un movimiento mayor al permitido\n");
+
                             }else {
+                                //Asignamos los valores para posicionar al jugador en su nueva posición
                                 int nuevaPosicionX = jugador.getPosicion().getPosX() + numMovX;
                                 int nuevaPosicionY = jugador.getPosicion().getPosY() + numMovY;
+
+                                /* Validamos que la posición esté dentro de los parámetros de anchura y altura del mapa,
+                                también verificamos si la nueva posición coincide con la salida para poder moverse hasta ella y ganar
+                                 */
                                 if ((nuevaPosicionY > 0 && nuevaPosicionX > 0) && (nuevaPosicionY < Habitacion.ALTO-1 && nuevaPosicionX < Habitacion.ANCHO -1) ||
                                         (habitacion.getPuertaSalida().getPosX() == nuevaPosicionX) &&
                                                 habitacion.getPuertaSalida().getPosY() == +nuevaPosicionY){
                                     jugador.moverse(nuevaPosicionX,nuevaPosicionY);
+
+                                    //Si el jugador ha entrado en la puerta de salida lanzamos la excepción
                                     if (jugador.haSalido()) throw new JugadorGana("Has ganado!!");
+
                                 }else{
                                     System.out.println("Posición no posible. ");
                                 }
